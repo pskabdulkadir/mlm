350f11c06206f6096518e869c874642824beda29
pskabdulkadir <psikologabdulkadirkan@gmail.com>
2026-06-17 01:32:50 +0300

    Add React Router v7 future flags to fix state transition warnings

--- mlm-app/client/App.tsx
+++ mlm-app/client/App.tsx
@@ -43,7 +43,7 @@ const App = () => (
       <Sonner />
       <ErrorBoundary>
         <Suspense fallback={<LoadingScreen />}>
-          <BrowserRouter>
+          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/sistem" element={<Sistem />} />
